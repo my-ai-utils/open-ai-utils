ced6cb516d946771b8b2ed337559fd2a130bd512
amigin <amigin@gmail.com>
2025-06-09 21:41:09 +0300

    Added ability to read func data

--- src/http_chunked_body_reader/models.rs
+++ src/http_chunked_body_reader/models.rs
@@ -11,6 +11,30 @@ impl ChunkResultModel {
         let choices = self.choices.get(0)?;
         choices.delta.content.as_deref()
     }
+
+    pub fn get_func(&self) -> Option<(&str, &str)> {
+        let choices = self.choices.get(0)?;
+        let tool_calls = choices.delta.tool_calls.as_ref()?;
+
+        let first_tool_call = tool_calls.get(0)?;
+
+        if let Some(function) = first_tool_call.function.as_ref() {
+            let name = if let Some(name) = function.name.as_ref() {
+                name.as_str()
+            } else {
+                ""
+            };
+
+            let args = if let Some(args) = function.arguments.as_ref() {
+                args.as_str()
+            } else {
+                ""
+            };
+
+            return Some((name, args));
+        }
+        None
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -27,9 +51,11 @@ pub struct ChunkChoseDelta {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ToolsCallModel {
     #[serde(rename = "type")]
-    r#type: String,
+    pub r#type: String,
+    pub function: Option<FunctionCallModel>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct FunctionCallModel {
     pub name: Option<String>,
     pub arguments: Option<String>,
