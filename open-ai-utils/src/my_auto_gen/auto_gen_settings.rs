2bd94a8f5b2097e3035517d015cd4da0a4d98c80
amigin <amigin@gmail.com>
2025-06-27 12:01:02 +0300

    Minor Improvements

--- open-ai-utils/src/my_auto_gen/auto_gen_settings.rs
+++ open-ai-utils/src/my_auto_gen/auto_gen_settings.rs
@@ -1,5 +1,17 @@
+use rust_extensions::StrOrString;
+
 pub struct AutoGenSettings {
-    pub url: String,
+    pub url: StrOrString<'static>,
     pub api_key: Option<String>,
     pub do_not_reuse_connection: Option<bool>,
+}
+
+impl AutoGenSettings {
+    pub fn create_as_open_ai(api_key: Option<String>, do_not_reuse_connection: bool) -> Self {
+        Self {
+            url: "https://api.openai.com/v1/chat/completions".into(),
+            api_key,
+            do_not_reuse_connection: Some(do_not_reuse_connection),
+        }
+    }
 }
